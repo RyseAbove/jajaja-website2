b961bdc174c69d678602cafc7a94a8b924a8b96b
Noah Layton <116219668+RyseAbove@users.noreply.github.com>
2024-03-22 21:51:37 +0000

    Fri, Mar 22, 2024, 2:51 PM -07:00

--- css-and-js/breakout.js
+++ css-and-js/breakout.js
@@ -133,12 +133,18 @@ function keyDown(e) {
     if (e.key == 'ArrowRight' || e.key == 'Right') {
         paddle.dx = paddle.speed
     }
+    if (e.key == 'ArrowLeft' || e.key == 'Left') {
+        paddle.dx = -paddle.speed
+    }
 }
 
 
 // keyup
 function keyUp(e) {
-    if (e.key == 'ArrowRight' || e.key == 'Right') {
+    if (e.key == 'ArrowRight' ||
+    e.key == 'Right' ||
+    e.key == 'ArrowLeft' ||
+    e.key == 'Left') {
         paddle.dx = 0
     }
 }
